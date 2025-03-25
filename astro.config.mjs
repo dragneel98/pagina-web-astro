0545f9cc4c943c08861aff53e09abb5e72057a05
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-25 04:15:22 -0300

    Agregar configuración inicial de Astro y estructura de proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Your configuration options here
+  site: 'https://gonzalomina.dev',
+  // Enable static site generation
+  output: 'static',
+});
