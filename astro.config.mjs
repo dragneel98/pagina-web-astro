b1457cac52a52a500b8ed69a41dc2f0692045edd
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-26 00:29:09 -0300

    Actualizar configuración del sitio y agregar flujo de trabajo para despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Your configuration options here
-  site: 'https://gonzalomina.dev',
+  site: 'https://dragneel98.github.io',
   // Enable static site generation
   output: 'static',
 });
