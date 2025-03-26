1aae36c18ad25345c51b4e895c2ec992bc8280bd
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-26 02:47:56 -0300

    Actualizar la URL del sitio en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Your configuration options here
-  site: 'https://dragneel98.github.io/pagina-web-astro',
+  site: 'https://dragneel98.github.io/',
   // Enable static site generation
   base: '/pagina-web-astro/',
   output: 'static',
