a92bb4a29d063777df1255689adab0a0abedcc74
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2025-03-26 02:31:40 -0300

    Configurar Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Your configuration options here
-  site: 'https://dragneel98.github.io',
+  site: 'https://dragneel98.github.io/pagina-web-astro',
   // Enable static site generation
   base: '/pagina-web-astro/',
   output: 'static',
